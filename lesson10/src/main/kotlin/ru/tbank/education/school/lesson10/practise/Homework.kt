8aa1dae2f6b72264ef1a915b64a2acc3eb522708
yaroslavsdev <yaroslav.safronov.09@yandex.ru>
2026-01-28 22:43:35 +0300

    Покрыты базовые сценарии из Д/з 10

--- lesson10/src/main/kotlin/ru/tbank/education/school/lesson10/practise/Homework.kt
+++ lesson10/src/main/kotlin/ru/tbank/education/school/lesson10/practise/Homework.kt
@@ -1,5 +1,10 @@
 package ru.tbank.education.school.lesson10.practise
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+
+
 /*
 ### Задание - проверка расписания
 
@@ -93,10 +98,74 @@ logs = [
 ```
 */
 
-fun normalize(log: String): String {
-    var str: String = log
-    str = str.lowercase()
-    return str
+data class LogResponse(   // Для ответов после нормализации
+    val dt: String,
+    val id: Int,
+    val status: String
+)
+
+data class DeliveryResult(   // Итоговый ответ
+    val id: Int,
+    val durationMinutes: Int,
+    val isLate: Boolean
+)
+
+fun createResponse(year: String, month: String, day: String, hours: String, minutes: String, id: String, status: String): LogResponse {
+    val currentDate: String = "${year}-${month}-${day} ${hours}:${minutes}"
+    val currentId: Int = id.toInt()
+    val currentStatus: String = status.lowercase()
+    return LogResponse(currentDate, currentId, currentStatus)
+}
+
+fun normalize(log: String): LogResponse? {
+    val currentLog = log.lowercase().replace(" ", "")
+
+    // Формат A: "2026-01-22 09:14 | ID:042 | STATUS:sent"
+    val patternA = Regex("""(\d{4})-(\d{2})-(\d{2})(\d{2}):(\d{2})\|id:(\d+)\|status:(sent|delivered)""")
+
+    // Формат B: "TS=22/01/2026-09:27; status=delivered; #042"
+    val patternB = Regex("""ts=(\d{2})/(\d{2})/(\d{4})-(\d{2}):(\d{2});status=(sent|delivered);#(\d+)""")
+
+    // Формат C: "[22.01.2026 09:40] delivered (id:044)"
+    val patternC = Regex("""\[(\d{2})\.(\d{2})\.(\d{4})(\d{2}):(\d{2})\](sent|delivered)\(id:(\d+)\)""")
+
+    var m: MatchResult? = patternA.matchEntire(currentLog)
+    if (m != null) {
+        val year = m.groupValues[1]
+        val month = m.groupValues[2]
+        val day = m.groupValues[3]
+        val hours = m.groupValues[4]
+        val minutes = m.groupValues[5]
+        val id = m.groupValues[6]
+        val status = m.groupValues[7]
+        return createResponse(year, month, day, hours, minutes, id, status)
+    }
+
+    m = patternB.matchEntire(currentLog)
+    if (m != null) {
+        val year = m.groupValues[3]
+        val month = m.groupValues[2]
+        val day = m.groupValues[1]
+        val hours = m.groupValues[4]
+        val minutes = m.groupValues[5]
+        val id = m.groupValues[7]
+        val status = m.groupValues[6]
+        return createResponse(year, month, day, hours, minutes, id, status)
+    }
+
+    m = patternC.matchEntire(currentLog)
+    if (m != null) {
+        val year = m.groupValues[3]
+        val month = m.groupValues[2]
+        val day = m.groupValues[1]
+        val hours = m.groupValues[4]
+        val minutes = m.groupValues[5]
+        val id = m.groupValues[7]
+        val status = m.groupValues[6]
+        return createResponse(year, month, day, hours, minutes, id, status)
+    }
+
+    return null
 }
 
 fun main() {
@@ -111,4 +180,80 @@ fun main() {
     }
 
     val normalized = logs.map { normalize(it) }
+    // normalized.forEach { println(it) }
+
+    val actions = mutableMapOf<Int, MutableList<LogResponse>>()
+    normalized.forEach {
+        if (it != null) {
+            if (actions.contains(it.id)) {
+                actions[it.id]!!.add(it)
+            } else {
+                actions[it.id] = mutableListOf<LogResponse>(it)
+            }
+        }
+    }
+
+    val result = mutableListOf<DeliveryResult>()
+    val formatError = mutableListOf<Int>()
+    val logicError = mutableListOf<Int>()  // Если "отправка" позже "доставлено"
+
+    for (action in actions) {
+        if (action.value.size != 2) {
+            formatError.add(action.key)
+        } else {
+            val sentTime = action.value.find {it.status == "sent"}
+            val deliveredTime = action.value.find {it.status == "delivered"}
+
+            if (sentTime == null || deliveredTime == null) {
+                formatError.add(action.key)
+                continue
+            }
+
+            val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+            val sentTimeFormatted = LocalDateTime.parse(sentTime.dt, dateFormatter)
+            val deliveredTimeFormatted = LocalDateTime.parse(deliveredTime.dt, dateFormatter)
+
+            val finalTime = ChronoUnit.MINUTES.between(sentTimeFormatted, deliveredTimeFormatted)
+            if (finalTime < 0) {
+                logicError.add(action.key)
+                continue
+            }
+
+            val push = DeliveryResult(action.key, finalTime.toInt(), finalTime > 20)
+            result.add(push)
+        }
+    }
+
+    if (result.size > 0) {
+        val sortedResult = result.sortedByDescending { it.durationMinutes }
+        println("Список всех доставок: (id | delivery time):")
+        sortedResult.forEach { println("id: ${it.id}, duration: ${it.durationMinutes}m") }
+
+        println("-----------------------")
+
+        println("Самый долгий заказ - id: ${sortedResult[0].id}, duration: ${sortedResult[0].durationMinutes}m")
+
+        println("-----------------------")
+    }
+
+    var shouldShow: Boolean = false
+    for (i in result) {
+        if (i.isLate) {
+            shouldShow = true
+            break
+        }
+    }
+    if (shouldShow) {
+        println("Нарушено условие доставки:")
+        result.forEach { if (it.isLate) println("id: ${it.id}, duration: ${it.durationMinutes}m") }
+    }
+
+    if (formatError.isNotEmpty()) {
+        println("Ошибки формата (неполные данные): ${formatError.joinToString(", ")}")
+    }
+
+    if (logicError.isNotEmpty()) {
+        println("Ошибки логики (доставка раньше отправки): ${logicError.joinToString(", ")}")
+    }
+
 }
